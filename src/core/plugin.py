95246d845ce669f0b2e29ba45bbcdf1c935a2f5d
Ki-Seki <Ki_Seki@outlook.com>
2023-05-05 20:44:41 +0800

    change `arxiv_today` to `arxiv_yesterday`

--- src/core/plugin.py
+++ src/core/plugin.py
@@ -3,14 +3,14 @@ This file store all plugins.
 """
 
 
-
 # Register new plugin here. One line for each plugin.
 __all__ = [
     'gen_shortcut', 
     'focus',
     'chat',
-    'arxiv_today'
+    'arxiv_yesterday'
     ]
+
 
 def gen_shortcut(dst="."):
     """
@@ -74,45 +74,56 @@ def chat(prompt):
     )
     return completion.choices[0].text
 
-def arxiv_today(kw, max_results=float('inf'), show_abstract=True, download=True, path='.'):
+def arxiv_yesterday(kw:str, max_results=-1, show_abstract=True, download=True, path='.'):
     """
-    Fetch the arXiv paper whose title or abstract contains `kw`.
+    Fetch the arXiv paper whose title or abstract contains `kw` yesterday.
 
-    * Only fetched papers submitted today.
-    * Paper abstract will be printed if `show_results=True`
+    * Fetch papers firstly submitted yesterday.
+    * Fetch at most `max_results` papers. Fetch all possible papers if `max_results=-1`.
+    * Paper abstract will be printed if `show_abstract=True`
     * Papers will be downloaded if `download=True`
     * Downloaded papers will be saved in `path`.
 
     Requirements:
     * `pip install arxiv`
+
+    More:
+    * arXiv official API: https://info.arxiv.org/help/api/index.html
     """
 
     import os
+    import datetime
     import arxiv
 
-    search = arxiv.Search(
-    query = kw,
-    max_results = max_results,
-    sort_by = arxiv.SortCriterion.SubmittedDate
-    )
+    query = f'ti:{kw} OR abs:{kw}'  # Title or abstract contains `kw`
+    search = arxiv.Search(query=query, sort_by=arxiv.SortCriterion.SubmittedDate)
+    print('Fetching papers...')
     papers = list(search.results())
+
+    # Ensure the published date is yesterday
+    year, month, yesterday = datetime.date.today().year, \
+                       datetime.date.today().month, \
+                       datetime.date.today().day - 1
+    papers = filter(lambda x: (x.published.year==year 
+                               and x.published.month==month 
+                               and x.published.day==yesterday)
+                               , papers)
+    
+    papers = list(papers)
+    papers = papers[:max_results] if max_results != -1 else papers
     cnt = len(papers)
+    print(f'Found {cnt} papers.')
 
     for i, paper in enumerate(papers):
         print(f'TITLE: {paper.title}')
         print(f'LINK: {paper.links[0]}')
-        if show_abstract:
-            print(f'ABSTRACT: {paper.summary}')
+        print(f'ABSTRACT: {paper.summary}') if show_abstract else ()
         if download:
-            if not os.path.exists(path):
-                os.mkdir(path)
+            os.mkdir(path) if not os.path.exists(path) else ()
             paper.download_pdf(dirpath=path, filename=f'{paper.get_short_id()}.pdf')
             print(f'Paper downloaded {i+1}/{cnt}.')
         print()
-    return 
 
         
-
 if __name__ == '__main__':
-    arxiv_today('LLM', 5, path='./papers/')
-    # print(arxiv_today.__doc__)
+    arxiv_yesterday('LLM', path='./papers/')
